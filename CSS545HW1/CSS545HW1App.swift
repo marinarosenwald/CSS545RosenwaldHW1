7f20dae06647d8b5c1a4ea70cfd2fe9fe4053173
marinarosenwald <marinarosenwald@gmail.com>
2024-04-06 17:18:43 -0700

    Initial Commit

---
+++ CSS545HW1/CSS545HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  CSS545HW1App.swift
+//  CSS545HW1
+//
+//  Created by Mari Rosenwald on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CSS545HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSS545HW1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CSS545HW1
+//
+//  Created by Mari Rosenwald on 4/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
